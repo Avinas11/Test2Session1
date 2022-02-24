2aa62c015eee7e3a2c54f6d8298dc336054f5674
Avinas11 <94587655+Avinas11@users.noreply.github.com>
2022-02-24 04:56:52 +0000

    test-2 practice

---
+++ p3original.c
@@ -0,0 +1,40 @@
+include <stdio.h>
+int input_n()
+{
+  int n;
+  printf("enter n value\n:");
+  scanf("%d",&n);
+  return n;
+}
+int is_prime(int n)
+{
+  int i,prime;
+  for(i=2;i<=n/2;i++)
+    {
+      if (n==0 && n==1)
+      return 1;
+      else if
+      (n%i ==0)
+      return 0;
+    }
+return prime;
+  }
+void output(int n,int prime)
+{
+  if(prime==0)
+  {
+    printf("entered number is not prime\n");
+  }
+  else 
+    {
+      printf("entered number is prime\n");
+    }
+}
+int main()
+{
+  int n,d;
+  n=input_n();
+  d=is_prime(n);
+  output(n,d);
+  return 0;
+}
